c1fc782674944efe64a7be7033e1af4eafcec81c
Ian Macinnes <ian.macinnes@gmail.com>
2013-03-26 15:02:52 +0700

    Initial commit.

---
+++ README.md
@@ -0,0 +1,16 @@
+# Very Simple Genetic Algorithm in Python
+
+I wanted to write the simplest genetic algorithm program I could that was still useful.
+This means it had to have abstract genotypes, mutation operators, and trials.
+So I had to use either Python or LISP, and Python won.
+
+The example shown creates a population of numbers and evolves a population with lower numbers.
+Pretty basic.
+
+I got it down to 17 lines of easy-to-read Python and I'd like to hear very much if anyone
+can do it in less without cheating.
+
+## Contributors
+
+ian.macinnes@gmail.com http://www.ianmacinnes.net
+andy@andybak.net https://github.com/andybak
---
+++ evolve.py
@@ -0,0 +1,54 @@
+import random
+import bisect
+
+# These can be replaced to produce different genotypes, mutations, and trials
+
+def initialise_genotypes():
+    return (int(10 * random.random()) for i in xrange(10))
+
+def mutate(i):
+    return i + random.randint(-1, 1)
+
+def trial(i):
+    return -i
+
+##################################################
+
+def run_trials(population, trial_fn):
+    return [(trial_fn(x),x) for x in population]
+
+def create_next_population(population_with_fitness, mutate_fn):
+    population_with_fitness.sort(key=lambda k : (k[0]))
+    sorted_population = [x[1] for x in population_with_fitness]
+
+    rank_selection = []
+    j = 0
+    max_relative_fitness = 0
+    for i in xrange(len(sorted_population)):
+        j += 1
+        max_relative_fitness += j
+        rank_selection.append(max_relative_fitness)
+
+    new_population = []
+    for i in xrange(len(sorted_population)):
+        j = bisect.bisect(rank_selection, random.random() * max_relative_fitness)
+        mutated_member = mutate_fn(sorted_population[j])
+        new_population.append(mutated_member)
+
+    return new_population
+
+def start_evolving(initialise_genotypes_fn, mutate_fn, trial_fn):
+    population = initialise_genotypes_fn()
+
+    for i in xrange(1000): # 1000 generations
+        population_with_fitness = run_trials( population, trial_fn )
+        population = create_next_population( population_with_fitness, mutate_fn )
+
+    for i in population:
+        print i
+
+def main():
+    start_evolving(initialise_genotypes, mutate, trial)
+
+if __name__ == "__main__":
+    main()
